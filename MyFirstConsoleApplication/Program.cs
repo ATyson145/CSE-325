3b24ac8567aa620417f13affed6baed3a9382d76
Adam <tys17001@gmail.com>
2023-09-24 23:04:49 -0600

    Week 1 Middle

--- MyFirstConsoleApplication/Program.cs
+++ MyFirstConsoleApplication/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System;
+
+namespace Week1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Adam Tyson";
+            string location = "Idaho";
+
+            Console.WriteLine($"My name is {myName}");
+            Console.WriteLine($"I am from {location}");
+            DateTime currentDate = DateTime.Now;
+            string date = currentDate.ToString("MM/dd/yyyy");
+            Console.WriteLine($"The current date is {date}");
+        }
+    }
+}
