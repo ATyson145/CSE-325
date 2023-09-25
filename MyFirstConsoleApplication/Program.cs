6a9e2fd7a073cf0998eac9624744531b1c6891c8
Adam <tys17001@gmail.com>
2023-09-24 23:31:58 -0600

    Week 1 Complete

--- MyFirstConsoleApplication/Program.cs
+++ MyFirstConsoleApplication/Program.cs
@@ -11,9 +11,33 @@ namespace Week1
 
             Console.WriteLine($"My name is {myName}");
             Console.WriteLine($"I am from {location}");
+
             DateTime currentDate = DateTime.Now;
             string date = currentDate.ToString("MM/dd/yyyy");
             Console.WriteLine($"The current date is {date}");
+
+            DateTime christmas = new DateTime(currentDate.Year, 12, 25);
+            TimeSpan timeUntilChristmas = christmas - currentDate;
+            int daysUntilChristmas = (int)timeUntilChristmas.TotalDays;
+            Console.WriteLine($"The number of days before Christmas is {daysUntilChristmas}");
+
+
+            Console.Write("Enter the width (in feet): ");
+            string width = Console.ReadLine();
+            double actWidth = double.Parse(width);
+
+            Console.Write("Enter the height (in feet): ");
+            string height = Console.ReadLine();
+            double actHeight = double.Parse(height);
+
+            double woodLength = 2 * (actWidth + actHeight) * 3.25;
+            double glassArea = 2 * (actWidth * actHeight);
+
+            Console.WriteLine($"The length of the wood is {woodLength}");
+            Console.WriteLine($"The area of the glass is {glassArea}");
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
         }
     }
 }
